63e5d9d1cf053adac5163362391d86412d9dd5e5
Nerijus Areska <nerijus@adroiti.com>
2015-07-09 20:00:52 +0300

    Add missing corp login button to dashboard #4

--- src/main/resources/loginAuth.js
+++ src/main/resources/loginAuth.js
@@ -1,8 +1,28 @@
 AJS.$(function() {
 
     if (AJS.$("#login-form").length) {
-	AJS.$("#login-form").hide();
-	AJS.$('<div class="field-group"><a class="aui-button aui-style aui-button-primary" href="plugins/servlet/saml/auth" style="align:center;">Use Corporate login</a></div><h2 style="margin-top:10px"></h2>').insertBefore(AJS.$("#login-form .form-body"));
+        loadCorpLogin(AJS.$("#login-form"));
+    } else {
+     AJS.$("iframe").ready(function () {
+
+       var iframe = AJS.$("#gadget-0")
+       iframe.load(function() {
+         loginForm = AJS.$("#" + iframe[0].id).contents().find("#loginform")
+         loadCorpLogin(loginForm);
+       });
+      });
+    }
+      
+ function loadCorpLogin(loginForm) {
+    if (loginForm.length == 1) {
+        loginFormId = loginForm[0].id
+	loginForm.hide();
+
+	if(loginFormId == "login-form") {
+            AJS.$('<div class="field-group"><a class="aui-button aui-style aui-button-primary" href="plugins/servlet/saml/auth" style="align:center;">Use Corporate login</a></div><h2 style="margin-top:10px"></h2>').insertBefore(AJS.$("#" + loginFormId + " .form-body"));
+        } else {
+            AJS.$('<div class="field-group"><a class="aui-button aui-style aui-button-primary" href="/jira/plugins/servlet/saml/auth" style="margin-left:100px;margin-top:5px;">Use Corporate login</a></div>').insertBefore(AJS.$("#gadget-0"));
+        }
 
         var query = location.search.substr(1);
         query.split("&").forEach(function(part) {
@@ -12,9 +32,9 @@ AJS.$(function() {
                 errorKeys["general"] = "General SAML configuration error";
                 errorKeys["user_not_found"] = "User was not found";
                 errorKeys["plugin_exception"] = "SAML plugin internal error";
-                AJS.$("#login-form").show();
+                loginForm.show();
 		var message = '<div class="aui-message closeable error">' + errorKeys[item[1]] + '</div>';
-                AJS.$(message).insertBefore(AJS.$("#login-form"));
+                AJS.$(message).insertBefore(loginForm);
             }
         });
 
@@ -26,7 +46,7 @@ AJS.$(function() {
                             },
                     success: function (response) {
                         if (response != "") {
-			    AJS.$('<p>Please wait while we redirect you to your company log out page</p>').insertBefore(AJS.$("#login-form"));
+			    AJS.$('<p>Please wait while we redirect you to your company log out page</p>').insertBefore(loginForm);
 			    window.location.href = response;
 			    return;
                         }
@@ -43,15 +63,16 @@ AJS.$(function() {
                     success: function (response) {
                         if (response=="true") {
                         	// AJS.$('<img src="download/resources/com.bitium.confluence.SAML2Plugin/images/progress.png"/>').insertBefore(AJS.$(".aui.login-form-container"));
-            				AJS.$('<p>Please wait while we redirect you to your company log in page</p>').insertBefore(AJS.$("#login-form"));
+            				AJS.$('<p>Please wait while we redirect you to your company log in page</p>').insertBefore(loginForm);
             				window.location.href = 'plugins/servlet/saml/auth';
 			        
 			} else {
-				AJS.$("#login-form").show();
+				loginForm.show();
 			}
                     }
                });
 
+         }
      }
 
 });
